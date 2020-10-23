09349c414f9c188817c9179a340f75fb1bd5f1c4
Ovdenser <ovdenser@gmail.com>
2020-10-23 10:43:14 +0300

    Homework 6

---
+++ src/Lesson6/Dog.java
@@ -0,0 +1,8 @@
+package Lesson6;
+
+public class Dog extends Animal {
+
+    Dog(String name, float maxJamp, float maxRun, float maxSwim) {
+        super("Dog", name, maxJamp, maxRun, maxSwim);
+    }
+}
